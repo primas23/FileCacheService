cce7b9a0b327dfb1e6b4ae1459f7bdb7ae666254
Primas <primas.petrov@gmail.com>
2017-10-02 22:10:32 +0300

    Added file cache config

--- FCS,ConsoleClient/Startup.cs
+++ FCS,ConsoleClient/Startup.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FCS_ConsoleClient
+﻿namespace FCS_ConsoleClient
 {
     public class Startup
     {
         public static void Main()
         {
-
         }
     }
 }
