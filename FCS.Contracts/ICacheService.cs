5692a99c87c17ed177e1615cbf5bee103c684fb4
Primas <primas.petrov@gmail.com>
2017-10-02 22:00:59 +0300

    Added cache architecture

---
+++ FCS.Contracts/ICacheService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FCS.Contracts
+{
+    public interface ICacheService
+    {
+        /// <summary>
+        /// Gets the value of the specified item name if exists or inserts it in the cache.
+        /// </summary>
+        /// <typeparam name="T">Any type</typeparam>
+        /// <param name="itemName">Name of the item.</param>
+        /// <param name="getDataFunc">The get data function.</param>
+        /// <param name="durationInSeconds">The duration in seconds.</param>
+        /// <returns>The cached item value</returns>
+        T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds);
+
+        /// <summary>
+        /// Removes the specified item name.
+        /// </summary>
+        /// <param name="itemName">Name of the item.</param>
+        void Remove(string itemName);
+
+        /// <summary>
+        /// Clears the all the cache.
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ FCS.Core/FileCache.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using FCS.Contracts;
+
+namespace FCS.Core
+{
+    public class FileCache : ICacheService
+    {
+        public T Get<T>(string itemName, Func<T> getDataFunc, int durationInSeconds)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(string itemName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
